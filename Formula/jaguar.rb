d88c1f95e945d7a3c34d02685b15ad10484f1efd
Florian Loitsch <florian@toit.io>
2025-03-17 15:02:00 +0100

    Add warning to use the core Jaguar. (#14)

---
+++ Formula/jaguar.rb
@@ -0,0 +1,18 @@
+# Copyright (C) 2025 Toit contributors
+# Use of this source code is governed by an MIT-style license that can be
+# found in the LICENSE file.
+
+class Jaguar < Formula
+  desc "Live reloading for your ESP32"
+  homepage "https://github.com/toitlang/jaguar"
+  url "https://github.com/toitlang/jaguar/archive/refs/tags/v1.48.0.tar.gz"
+  sha256 "fb5a329ef5c166a791c84adf4a9c3c8003c974fe58384d428bde84baf1ec7493"
+  license "MIT"
+  head "https://github.com/toitlang/jaguar.git", branch: "main"
+  disable! date: "2025-03-16", because: \
+    "This formula has been migrated to Homebrew/core. Please run: brew install jaguar"
+
+  def install
+    odie "This formula has been migrated to Homebrew/core. Please run: brew install jaguar"
+  end
+end
