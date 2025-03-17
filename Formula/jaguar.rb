a9f1e77da147d449dd68ebe75067d27c80ff8c11
Florian Loitsch <florian@loitsch.com>
2025-03-17 11:59:21 +0100

    Move "disable" down.

--- Formula/jaguar.rb
+++ Formula/jaguar.rb
@@ -4,12 +4,12 @@
 
 class Jaguar < Formula
   desc "Live reloading for your ESP32"
-  disable! because: "This formula has been migrated to Homebrew/core. Please run: brew install jaguar"
   homepage "https://github.com/toitlang/jaguar"
   url "https://github.com/toitlang/jaguar/archive/refs/tags/v1.48.0.tar.gz"
   sha256 "fb5a329ef5c166a791c84adf4a9c3c8003c974fe58384d428bde84baf1ec7493"
   license "MIT"
   head "https://github.com/toitlang/jaguar.git", branch: "main"
+  disable! because: "This formula has been migrated to Homebrew/core. Please run: brew install jaguar"
 
   def install
     odie "This formula has been migrated to Homebrew/core. Please run: brew install jaguar"
