28ddfc022d9cf10f973f5f2b11a344a657c0e090
Florian Loitsch <florian@loitsch.com>
2025-03-17 11:50:05 +0100

    More action updates.

--- Formula/jaguar.rb
+++ Formula/jaguar.rb
@@ -4,6 +4,12 @@
 
 class Jaguar < Formula
   disable! "This formula has been migrated to Homebrew/core. Please run: brew install jaguar"
+  desc "Live reloading for your ESP32"
+  homepage "https://github.com/toitlang/jaguar"
+  url "https://github.com/toitlang/jaguar/archive/refs/tags/v1.48.0.tar.gz"
+  sha256 "fb5a329ef5c166a791c84adf4a9c3c8003c974fe58384d428bde84baf1ec7493"
+  license "MIT"
+  head "https://github.com/toitlang/jaguar.git", branch: "main"
 
   def install
     odie "This formula has been migrated to Homebrew/core. Please run: brew install jaguar"
