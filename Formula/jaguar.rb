c8c70e4c2434e2ba848113a44c8e9f61d5561ca5
Florian Loitsch <florian@loitsch.com>
2025-03-17 11:43:35 +0100

    Add warning to use core jaguar.

---
+++ Formula/jaguar.rb
@@ -0,0 +1,11 @@
+# Copyright (C) 2025 Toit contributors
+# Use of this source code is governed by an MIT-style license that can be
+# found in the LICENSE file.
+
+class Jaguar < Formula
+  disable! "This formula has been migrated to Homebrew/core. Please run: brew install jaguar"
+
+  def install
+    odie "This formula has been migrated to Homebrew/core. Please run: brew install jaguar"
+  end
+end
