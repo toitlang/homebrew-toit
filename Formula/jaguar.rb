e62a557d7784c07ad95975c171d4bcccf9786fcb
Leon Gungadin Mogensen <leon@toit.io>
2025-04-01 08:12:04 +0000

    Update to v1.50.3

--- Formula/jaguar.rb
+++ Formula/jaguar.rb
@@ -5,8 +5,8 @@
 class Jaguar < Formula
   desc "Live reloading for your ESP32"
   homepage "https://github.com/toitlang/jaguar"
-  url "https://github.com/toitlang/jaguar/archive/refs/tags/v1.50.2.tar.gz"
-  sha256 "a41109c49a2eeb0cbaffa752353968194867506aee9476a1e8b46184e1ad5fd4"
+  url "https://github.com/toitlang/jaguar/archive/refs/tags/v1.50.3.tar.gz"
+  sha256 "d3ce88689b316cae68f52a74cd8d3644be736ff3d8f2f6635c0cd2790d72b942"
   license "MIT"
   head "https://github.com/toitlang/jaguar.git", branch: "main"
   disable! date: "2025-03-16", because: \
